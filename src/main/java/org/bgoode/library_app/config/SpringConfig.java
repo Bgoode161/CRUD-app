378c19ebe8261a0491f1f61131ed781aa6d8103a
Bgoode161 <mafiosy344113@gmail.com>
2022-12-19 11:44:20 +0100

    Update SpringConfig.java

--- src/main/java/org/bgoode/library_app/config/SpringConfig.java
+++ src/main/java/org/bgoode/library_app/config/SpringConfig.java
@@ -38,20 +38,22 @@ public class SpringConfig implements WebMvcConfigurer {
         templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/views/");
         templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
         return templateResolver;
     }
     @Bean
     public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
-        springTemplateEngine.setTemplateResolver(templateResolver());
-        springTemplateEngine.setEnableSpringELCompiler(true);
-        return templateEngine();
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
     }
 
     public void configureViewResolvers(ViewResolverRegistry registry) {
-        ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
-        thymeleafViewResolver.setTemplateEngine(templateEngine());
-        registry.viewResolver(thymeleafViewResolver);
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
+        registry.viewResolver(resolver);
     }
     @Bean
     public DataSource dataSource() {
